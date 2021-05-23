6b6b3c5170dfd8a957686a59c95ad6370c2cd61c
1152357479@qq.com <1152357479@qq.com>
2021-05-23 10:10:18 +0000

    test git action

--- grimm/main/views.py
+++ grimm/main/views.py
@@ -7,7 +7,7 @@ from grimm.utils import constants
 
 @main.route("/tags", methods=['GET'])
 def tags_db():
-    """view function to get all tags info"""
+    """view function to get all tags info 111"""
     tag_list = [{'tag_id': i, 'tag_name': constants.TAG_LIST[i]} for i in range(len(constants.TAG_LIST))]
     logger.info("query all tags info successfully")
     return jsonify(tag_list)
